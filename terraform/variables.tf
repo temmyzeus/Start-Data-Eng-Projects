f8cd5b08fe6219005e2cea5184e1f8202845f1e0
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2022-08-02 13:30:51 +0100

    Setup EC2 with tls keys

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,42 +1,47 @@
 variable "bucket_prefix" {
-  type = string
+  type        = string
   description = "Prefix name of S3 Bucket"
 }
 
 variable "profile" {
-  type = string
-  default = "default"
+  type        = string
+  default     = "default"
   description = "AWS Profile"
 }
 
 variable "region" {
-  type = string
-  default = "us-west-2"
+  type        = string
+  default     = "us-west-2"
   description = "AWS Resource Region"
 }
 
 variable "aws_config_path" {
-  type = tuple([string])
-  default = ["$HOME/.aws/config"]
+  type        = tuple([string])
+  default     = ["$HOME/.aws/config"]
   description = "AWS Configuration Path"
 }
 
 variable "aws_credentials_path" {
-  type = tuple([string])
-  default = ["$HOME/.aws/credentials"]
+  type        = tuple([string])
+  default     = ["$HOME/.aws/credentials"]
   description = "AWS Credentials Path"
 }
 
 variable "AIRFLOW_TAG" {
-  type = map
+  type        = map(any)
   description = "Tag for Resources used by Airflow"
   default = {
     Name = "Airflow_Instance"
-    App = "Airflow"
+    App  = "Airflow"
   }
 }
 
 variable "AZ" {
-  type = string
+  type        = string
   description = "Availability Zone(s)"
+}
+
+variable "public_key_path" {
+  type        = string
+  description = "Path to public key"
 }

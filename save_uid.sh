3fe3f51e0fd7757fd24d195e3be1fc01a3239e29
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2022-10-24 16:25:48 +0000

    fix bas substitution

--- save_uid.sh
+++ save_uid.sh
@@ -1,3 +1,3 @@
 echo -e "AIRFLOW_UID=$(id -u)" > begineer_batch_etl/.env
-echo -e "AIRFLOW_GID=${id -g}" >> begineer_batch_etl/.env
+echo -e "AIRFLOW_GID=$(id -g)" >> begineer_batch_etl/.env
 echo "UID and GIU saved to .env file"
